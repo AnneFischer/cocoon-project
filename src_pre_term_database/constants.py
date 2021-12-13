ce6d33c90017de9175b669558df54c378de5f8ce
AFischer <anne.fischer@pacmed.nl>
2021-12-13 12:10:33 +0100

    First commit

---
+++ src_pre_term_database/constants.py
@@ -0,0 +1,16 @@
+#Variable constants
+
+REC_ID_NAME: str = 'rec_id'
+AGE_NAME: str = 'age'
+PARITY_NAME: str = 'parity'
+ABORTIONS_NAME: str = 'abortions'
+WEIGHT_NAME: str = 'weight'
+HYPERTENSION_NAME: str = 'hypertension'
+DIABETES_NAME: str = 'diabetes'
+PLACENTAL_NAME: str ='placental_position'
+BLEEDING_FIRST_TRI_NAME: str = 'bleeding_first_trimester'
+BLEEDING_SEC_TRI_NAME: str = 'bleeding_second_trimester'
+FUNNELING_NAME: str = 'funneling'
+SMOKER_NAME: str = 'smoker'
+GESTATION_NAME: str = 'gestation'
+NONE_NAME: str = 'None'
