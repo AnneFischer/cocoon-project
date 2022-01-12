25b8a78e4f15f4c4b8625b717ddaee6ac93398ea
AFischer <AFischer@MacBook-Pro.local>
2022-01-12 17:29:14 +0100

    Moved function

---
+++ src_pre_term_database/modeling.py
@@ -0,0 +1,20 @@
+class SaveBestModel:
+    """
+    Save the best model while training. If the current epoch's
+    validation loss is less than the previous least less, then save the
+    model state.
+    """
+    def __init__(self, best_valid_loss=float('inf')):
+        self.best_valid_loss = best_valid_loss
+
+    def __call__(self, current_valid_loss, epoch, model, optimizer, criterion, output_path, file_name_output):
+        if current_valid_loss < self.best_valid_loss:
+            self.best_valid_loss = current_valid_loss
+            print(f"\nBest validation loss: {self.best_valid_loss}")
+            print(f"\nSaving best model for epoch: {epoch}\n")
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': criterion,
+            }, f'{output_path}/{file_name_output}.pth')
