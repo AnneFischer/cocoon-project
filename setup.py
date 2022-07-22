fa4312e7970256d12e970ceccf1ecde4e07d1bbb
AFischer <AFischer@MacBook-Pro-8.local>
2022-07-22 16:19:43 +0200

    Command line arguments for evaluation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cocoon-project',
+    version='0.1',
+    packages=['references'],
+    url='https://github.com/AnneFischer/cocoon-project',
+    license='',
+    author='AFischer',
+    author_email='annefischer@kpnmail.nl',
+    description='Code for the Cocoon project: Prediction of pre-term birth using a variety of data sources (including Electrohysterography) using machine learning.  '
+)
